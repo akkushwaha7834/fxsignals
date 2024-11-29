53b7ef6cd3f086ead1e3ee57c7a8af6e4f4a958c
akkushwaha7834 <akkushwaha018@gmail.com>
2024-11-29 12:37:55 +0530

    Demo Nav Bar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../assets/fxsignals-logo.webp'; // Adjust the path if needed
+
+function NavigationBar() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        {/* Logo with a clickable link */}
+        <Navbar.Brand as={Link} to="/">
+          <img 
+            src={logo} 
+            alt="FXSignals Logo" 
+            style={{ width: '100px', height: 'auto' }} // Adjust the size of the logo
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
