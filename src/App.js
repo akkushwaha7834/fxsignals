0c104bb2c166562cad6f6abecedce8e205a24966
akkushwaha7834 <akkushwaha018@gmail.com>
2024-11-29 16:22:37 +0530

    Style.css file created and added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Blog from './components/Blog';
+import './assets/css/style.css';
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import logo from '../assets/fxsignals-logo.png'; // Adjust the path if needed
+import logo from '../assets/images/fxsignals-logo.png'; // Adjust the path if needed
 
 function NavigationBar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        {/* Logo with a clickable link */}
+        {/* Logo aligned to the left */}
         <Navbar.Brand as={Link} to="/">
           <img 
             src={logo} 
@@ -15,9 +15,11 @@ function NavigationBar() {
             style={{ width: '100px', height: 'auto' }} // Adjust the size of the logo
           />
         </Navbar.Brand>
+        
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
+          {/* Links aligned to the right */}
+          <Nav className="ms-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/about">About</Nav.Link>
             <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
